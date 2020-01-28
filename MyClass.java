7b7c481dca2eb5311ad36be694070ef4519fe6d5
Vyshnavi2410 <60385577+Vyshnavi2410@users.noreply.github.com>
2020-01-28 15:56:09 +0530

    Myclass.java

---
+++ MyClass.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) {
+      
+      Scanner sc=new Scanner(System.in);
+      int n=sc.nextInt();
+      if(n>0)
+      {
+          System.out.println(" n is a positive number");
+      }
+    else if(n<0)
+    {
+        System.out.println(" n is a negative number");
+        
+    }
+    else
+    {
+        System.out.println(" n is neither negative or positive");
+    }
+
+     
+    }
+}
